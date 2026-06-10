b9273c1ce69ef2dfae4ce377ec11c998c334515f
shivashankar_damidi <shivashankar.damidi@gmail.com>
2026-06-10 12:42:18 +0530

    Implemented daily cleanup of unverified student registrations

--- src/main/java/com/dmantz/lms/LmsBApplication.java
+++ src/main/java/com/dmantz/lms/LmsBApplication.java
@@ -2,8 +2,10 @@ package com.dmantz.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LmsBApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/dmantz/lms/repository/StudentRepository.java
+++ src/main/java/com/dmantz/lms/repository/StudentRepository.java
@@ -1,11 +1,14 @@
 package com.dmantz.lms.repository;
 
 import com.dmantz.lms.entity.Student;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,6 +34,15 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     String findMaxStudentId();
 
 	boolean existsByStudentId(String studentId);
+
+    @Modifying
+    @Transactional
+    @Query("""
+        DELETE FROM Student s
+        WHERE s.enabled = 'N'
+        AND s.createdDt < :cutoffTime
+    """)
+    int deleteUnverifiedStudents(@Param("cutoffTime") LocalDateTime cutoffTime);
 
 }
 
